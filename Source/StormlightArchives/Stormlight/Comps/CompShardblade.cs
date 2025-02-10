517b1b6ba419c64dfcf337276826d543af8464c8
Jørgen W. S. <54083739+Luucid@users.noreply.github.com>
2025-02-10 11:05:51 +0100

    Update CompShardblade.cs

--- Source/StormlightArchives/Stormlight/Comps/CompShardblade.cs
+++ Source/StormlightArchives/Stormlight/Comps/CompShardblade.cs
@@ -12,9 +12,8 @@ namespace StormlightMod {
         }
 
         public bool isBonded() { return Props.owner != null; }
-        public void bondWithPawn(ref Pawn pawn, ref CompShardblade blade) {
+        public void bondWithPawn(ref Pawn pawn) {
             Props.owner = pawn;
-            Props.thisBladeThing = blade;
             Log.Message($"Shard bonded with {pawn.Name}"); 
         }
         public void severBond() {
@@ -26,11 +25,20 @@ namespace StormlightMod {
                 thisBladeThing = thisBlade;
             Props.owner.equipment.AddEquipment(thisBladeThing);
         }
+
+        public void createBlade(ref Pawn pawn) {
+            ThingDef stuffDef = DefDatabase<ThingDef>.GetNamed("ShardMaterial", true);
+            ThingWithComps equipment = (ThingWithComps)ThingMaker.MakeThing(Props.thingDef, stuffDef);
+            Props.thisBladeThing = equipment.Primary.GetComp<CompShardblade>();
+            Log.Message($"Radiant {pawn.Name} created his shard blade!");
+            if (blade.isBonded() == false) {
+                blade.bondWithPawn(ref pawn);
+            }
+        }
     }
 }
 namespace StormlightMod {
     public class CompProperties_Shardblade : CompProperties {
-        public bool isBondedByPawn;
         public bool isSpawned = false;
         public Pawn owner = null;
         public ThingWithComps thisBladeThing = null;
