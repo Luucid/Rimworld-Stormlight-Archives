cf7ffe5797e8a999a656dae6b2aed6abd8475592
Jørgen W. S. <54083739+Luucid@users.noreply.github.com>
2025-03-17 09:15:01 +0100

    Update HeatFabrial.cs
    move code to seperate file

--- Source/StormlightArchives/Stormlight/Components/Comps/Fabrials/HeatFabrial.cs
+++ Source/StormlightArchives/Stormlight/Components/Comps/Fabrials/HeatFabrial.cs
@@ -174,5 +174,88 @@ namespace StormlightMod {
             this.compClass = typeof(CompHeatrial);
         }
     }
+  public class CompBasicBuildingFabrial : ThingComp, IGemstoneHandler {
+        public CompProperties_BasicBuildingFabrial Props => (CompProperties_BasicBuildingFabrial)props;
+        public CompGlower GlowerComp => parent.GetComp<CompGlower>();
+        public Thing insertedGemstone = null;
+        public Thing gemstoneMetalCage = null; //custom def for metal cages
+        public bool PowerOn = false;
 
+        public override void PostSpawnSetup(bool respawningAfterLoad) {
+            base.PostSpawnSetup(respawningAfterLoad);
+        }
+
+        public override void PostExposeData() {
+            base.PostExposeData();
+        }
+
+        public void checkPower(bool flickeredOn) {
+            if (insertedGemstone != null) {
+                var stormlightComp = (insertedGemstone as ThingWithComps).GetComp<CompStormlight>();
+                if (stormlightComp != null) {
+                    PowerOn = stormlightComp.HasStormlight && flickeredOn;
+                    return;
+                }
+            }
+            PowerOn = false;
+        }
+
+        public void usePower() {
+            if (PowerOn && insertedGemstone != null && insertedGemstone.TryGetComp<CompStormlight>() is CompStormlight stormlightComp) {
+                stormlightComp.drainStormLight(7f);
+            }
+        }
+
+
+        public void AddGemstone(ThingWithComps gemstone) {
+            var gemstoneComp = gemstone.GetComp<CompCutGemstone>();
+            if (gemstoneComp != null && gemstoneComp.parent.def == StormlightModDefs.whtwl_CutRuby) {
+                insertedGemstone = gemstoneComp.parent;
+            }
+        }
+
+        public void RemoveGemstone() {
+            if (insertedGemstone != null) {
+                var gemstoneToDrop = insertedGemstone;
+                insertedGemstone = null;
+                IntVec3 dropPosition = parent.Position;
+                GenPlace.TryPlaceThing(gemstoneToDrop, dropPosition, parent.Map, ThingPlaceMode.Near);
+
+            }
+        }
+        public override string CompInspectStringExtra() {
+            string gemName = "No gem in fabrial.";
+
+            if (insertedGemstone != null) {
+                ThingWithComps gemstone = insertedGemstone as ThingWithComps;
+                gemName = gemstone.GetComp<CompCutGemstone>().GetFullLabel + "(" + gemstone.GetComp<CompStormlight>().Stormlight.ToString("F0") + ")";
+            }
+            return gemName;
+        }
+        public override IEnumerable<FloatMenuOption> CompFloatMenuOptions(Pawn selPawn) {
+            var cutGemstone = GenClosest.ClosestThing_Global(
+                   selPawn.Position,
+                   selPawn.Map.listerThings.AllThings.Where(thing => thing.def == StormlightModDefs.whtwl_CutRuby), 500f);
+
+            Action replaceGemAction = null;
+            string replaceGemText = "No suitable gem available";
+            if (cutGemstone != null) {
+
+                replaceGemAction = () => {
+                    Job job = JobMaker.MakeJob(StormlightModDefs.whtwl_RefuelFabrial, parent, cutGemstone);
+                    if (job.TryMakePreToilReservations(selPawn, errorOnFailed: true)) {
+                        selPawn.jobs.TryTakeOrderedJob(job);
+                    }
+                };
+                replaceGemText = $"Replace with {cutGemstone.Label}";
+            }
+            yield return new FloatMenuOption(replaceGemText, replaceGemAction);
+        }
+    }
+
+    public class CompProperties_BasicBuildingFabrial : CompProperties {
+        public CompProperties_BasicBuildingFabrial() {
+            this.compClass = typeof(CompBasicBuildingFabrial);
+        }
+    }
 }
